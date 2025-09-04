ccf77ebf59f416b1b410bcf2bc65e1db0287c364
luckykharb0324 <luckykharb0000gmail.com>
2025-09-04 11:57:11 +0530

    edit mother name field

--- models/Student.js
+++ models/Student.js
@@ -6,6 +6,7 @@ const studentSchema = new Schema({
     rollNO: { type: String, required: true },
     studentName: { type: String },
     fatherName: { type: String, required: true },
+    motherName: { type: String, },
     course: { type: String, required: true },
     branch: { type: String },
     yearOfAdmission: { type: String },
